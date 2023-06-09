d809bc82ad5131c5a5007e89671b70c9ddbaf4b1
Timothy Karani <kartimothy@gmail.com>
2023-06-09 14:33:20 +0300

    Checkpoint

--- app/Filament/Resources/CountryResource.php
+++ app/Filament/Resources/CountryResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources;
 
+use App\Filament\Resources\CountryResource\RelationManagers\EmployeesRelationManager;
 use App\Filament\Resources\CountryResource\Pages;
 use App\Filament\Resources\CountryResource\RelationManagers;
 use App\Models\Country;
@@ -61,7 +62,7 @@ class CountryResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            EmployeesRelationManager::class,
         ];
     }
 
