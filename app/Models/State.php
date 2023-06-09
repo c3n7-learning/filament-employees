810766af4054d92b3053efb826748b1e666945b3
Timothy Karani <kartimothy@gmail.com>
2023-06-09 03:45:22 +0300

    Added models

--- app/Models/State.php
+++ app/Models/State.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class State extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['country_id', 'name'];
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+
+    public function city()
+    {
+        return $this->hasMany(City::class);
+    }
 }
