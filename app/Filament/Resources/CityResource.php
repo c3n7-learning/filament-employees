bbd943e17247c76f5c76dfb33c4f961ed26855c6
Timothy Karani <kartimothy@gmail.com>
2023-06-09 14:17:57 +0300

    Nav listing

--- app/Filament/Resources/CityResource.php
+++ app/Filament/Resources/CityResource.php
@@ -23,6 +23,7 @@ class CityResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-office-building';
     protected static ?string $navigationGroup = 'System Management';
+    protected static ?int $navigationSort = 3;
 
     public static function form(Form $form): Form
     {
--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -22,6 +22,7 @@ class DepartmentResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
     protected static ?string $navigationGroup = 'System Management';
+    protected static ?int $navigationSort = 4;
 
     public static function form(Form $form): Form
     {
